51f573e86d3a758407916602cd7e9ddf53fee4dc
zach6585 <zmines@wesleyan.edu>
2020-12-08 00:44:10 -0500

    commit tree 7275cb9253393ad783160e5304ac61418a29c70d

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,31 @@
-class Ride < ActiveRecord::Base
+class Ride < ApplicationRecord
+    belongs_to :attraction
+    belongs_to :user
+    def take_ride 
+        a = ""
+        if self.user.tickets < self.attraction.tickets 
+            a += "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+        end
+        if self.user.height < self.attraction.min_height
+            if a != ""
+                a += " You are not tall enough to ride the #{self.attraction.name}."
+            else 
+                a += "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+            end 
+        end 
+        if a != ""
+            return a
+        else 
+            user = User.find(self.user_id)
+            # binding.pry
+            user.tickets -= self.attraction.tickets
+            user.nausea += self.attraction.nausea_rating
+            user.happiness += self.attraction.happiness_rating
+            user2 = User.find(self.user_id)
+            User.delete(self.user_id)
+            @a = User.create!(user.attributes)
+
+            return "Thanks for riding the #{self.attraction.name.titleize}!"
+        end 
+    end 
 end
